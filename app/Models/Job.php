9a6a6aae2e5b59d462d4d9d2eba879ae7b4574cc
adamusek-007 <adam.kuczabinski@gmail.com>
2025-07-19 09:37:55 +0200

    Convert Job to Eloquent

--- app/Models/Job.php
+++ app/Models/Job.php
@@ -1,33 +1,12 @@
 <?php
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Model;
 
-class Job {
-    public static function all(): array
-    {
-        return [
-            [
-                'id' => '1',
-                'title' => 'Directory',
-                'salary' => '$50,000'
-            ],
-            [
-                'id' => '2',
-                'title' => 'Programmer',
-                'salary' => '$100,000'
-            ],
-            [
-                'id' => '3',
-                'title' => 'Teacher',
-                'salary' => '$75,000'
-            ]
-        ];
-    }
 
-    public static function find(int $id): array
-    {
-        return Arr::first(static::all(), function ($job) use ($id) {
-            return $job['id'] == $id;
-        });
-    }
+class Job extends Model{
+    protected $table = 'job_listings';
+
+    protected $fillable = ['title', 'salary'];
+   
 }
