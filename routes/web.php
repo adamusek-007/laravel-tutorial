2b39a2ae1f712315fd9edf3927a50e0b70b40b9e
adamusek-007 <adam.kuczabinski@gmail.com>
2025-08-07 15:43:31 +0200

    Combine Route:controller to Route::rescource and Route:get to view

--- routes/web.php
+++ routes/web.php
@@ -5,20 +5,7 @@ use Illuminate\Support\Arr;
 use Illuminate\Support\Facades\Route;
 use App\Models\Job;
 
-Route::get('/', function () {
-    return view('home');
-});
+Route::view('/', 'home');
+Route::view('/contact', 'contact');
 
-Route::controller(JobController::class)->group(function () {
-    Route::get('/jobs', 'index');
-    Route::post('/jobs', 'store');
-    Route::get('/jobs/create', 'create');
-    Route::get('/jobs/{job}', 'show');
-    Route::get('/jobs/{job}/edit', 'edit');
-    Route::patch('/jobs/{job}', 'update');
-    Route::delete('/jobs/{job}', 'destroy');
-});
-
-Route::get('/contact', function () {
-    return view('contact');
-});
+Route::resource('jobs', JobController::class);
